06b469fb7ef7d7a6a14876a6438a6969f56a67f7
Russell Ratcliffe <rratcliffe57@gmail.com>
2017-10-21 13:12:25 -0400

    added twillio

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from flask import Flask
+from twilio.rest import Client
+
+
 app = Flask(__name__)
+
+# Your Account SID from twilio.com/console
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# Your Auth Token from twilio.com/console
+auth_token = "your_auth_token"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+15558675309",
+    from_="+15017250604",
+    body="Hello from Python!")
+
+print(message.sid)
 
 
 @app.route('/')
